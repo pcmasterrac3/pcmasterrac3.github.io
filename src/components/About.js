59fc880a2a2a4846191fd2847cd6f6cf7367165f
yozhikvtumane <toshamatveeev@gmail.com>
2019-11-12 18:17:51 +0100

    backup before adding router switch to fix about route

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from "react"
+import Header from './Header'
+import Footer from './Footer'
+
+
+class About extends Component {
+	render() {
+		return(
+			<Fragment>
+				<Header />
+				
+				<Footer />
+			</Fragment>
+		)
+	}
+}
+
+export default About
--- src/containers/RouteConfig.js
+++ src/containers/RouteConfig.js
@@ -1,14 +1,20 @@
 import React, { Fragment } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import MainBlogPage from '../components/Blog'
 import SingleArticle from '../components/SingleArticle'
+import About from '../components/About'
 
 export default function() {
 	return(
 		<Fragment>
 			<Router>
+				
 				<Route exact path="/">
 					<MainBlogPage />
+				</Route>
+				
+				<Route path="/about">
+					<About />
 				</Route>
 				<Route path="/:articleId"
 					   render={props => {
